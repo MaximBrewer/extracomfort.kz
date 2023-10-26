c315d7ff3720ed93705dcd6b3ab0c8f95c41a001
MaximBrewer <pimax1978@icloud.com>
2023-10-26 19:30:21 +0600

    Icon Facility

--- app/Http/Resources/FacilityTizer.php
+++ app/Http/Resources/FacilityTizer.php
@@ -16,9 +16,10 @@ class FacilityTizer extends JsonResource
     public function toArray(Request $request): array
     {
         $arr = parent::toArray($request);
+        $arr['icon'] = $arr['icon'] ? Voyager::image($arr['icon']) : "";
+        $arr['icondark'] = $arr['icondark'] ? Voyager::image($arr['icondark']) : "";
         $arr['bg'] = $arr['bg'] ? Voyager::image($arr['bg']) : "";
         $arr['image'] = $arr['image'] ? Voyager::image($arr['image']) : "";
-        // $arr['text6image'] = $arr['text6image'] ? Voyager::image($arr['text6image']) : "";
         return $arr;
     }
 }
