5c58cfd8426c70b355cf3d41ae2202d60f1484f8
MaximBrewer <pimax1978@icloud.com>
2023-11-23 07:49:27 +0600

    css

--- public/build/assets/Top-f30e24ca.js
+++
@@ -1,1 +0,0 @@
-import{q as r,a as e}from"./app-7425cd10.js";import{B as s}from"./Breadcrumbs-b1409f2c.js";const i=()=>{const a=r().props;return e("div",{className:"catalogue-categories",children:e("div",{className:"container-outer",children:e("div",{className:"catalogue-categories__outer",children:e("div",{className:"catalogue-categories__inner",children:e(s,{...a})})})})})};export{i as T};
