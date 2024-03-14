8a5299a49e917cf5268a3371b782e0b24c5b518c
MaximBrewer <pimax1978@icloud.com>
2024-03-14 12:54:52 +0600

    sort

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class ProductImage extends Model
 {
     use HasFactory;
-    
+
     protected $guarded = [];
+
+    /**
+     * Default sort by created_at desc.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('sort', 'asc');
+        });
+    }
 }
