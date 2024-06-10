e1c5fa786bb564fa62b46a9570972544d09ec379
MaximBrewer <pimax1978@icloud.com>
2024-06-10 12:33:27 +0600

    css

---
+++ public/build/assets/Top-00ac04db.js
@@ -0,0 +1,1 @@
+import{q as r,a as e}from"./app-f2ea6e7b.js";import{B as s}from"./Breadcrumbs-4901e0aa.js";const i=()=>{const a=r().props;return e("div",{className:"catalogue-categories",children:e("div",{className:"container-outer",children:e("div",{className:"catalogue-categories__outer",children:e("div",{className:"catalogue-categories__inner",children:e(s,{...a})})})})})};export{i as T};
