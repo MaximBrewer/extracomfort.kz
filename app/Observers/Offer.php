317a3d4c87d4c78b4f3611501268ff660ddfb749
MaximBrewer <pimax1978@icloud.com>
2024-06-10 12:28:56 +0600

    scsc

--- app/Observers/Offer.php
+++ app/Observers/Offer.php
@@ -27,16 +27,18 @@ class Offer
     private function setMinPrice(ModelsOffer $model)
     {
         $minPrice = 1000000000;
-        foreach ($model->product->offers as $offer) {
-            foreach ($offer->prices as $price) {
-                $minPrice = $minPrice > $price->value ? $price->value : $minPrice;
+        if ($model->product) {
+            foreach ($model->product->offers as $offer) {
+                foreach ($offer->prices as $price) {
+                    $minPrice = $minPrice > $price->value ? $price->value : $minPrice;
+                }
             }
+            if ($minPrice < 1000000000) {
+                $model->product->min_price = $minPrice;
+            }
+            $model->product->quantity = $model->product->offers()->where('quantity', '>', 0)->exists();
+            $model->product->saveQuietly();
         }
-        if ($minPrice < 1000000000) {
-            $model->product->min_price = $minPrice;
-        }
-        $model->product->quantity = $model->product->offers()->where('quantity', '>', 0)->exists();
-        $model->product->saveQuietly();
     }
 
     private function setFacets(ModelsOffer $model)
