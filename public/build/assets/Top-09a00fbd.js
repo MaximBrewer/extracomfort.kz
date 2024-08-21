95457672a2e1b6b3e99d6b23e520544864d9d2a7
MaximBrewer <pimax1978@icloud.com>
2024-08-21 14:27:12 +0600

    btn-primary

--- public/build/assets/Top-09a00fbd.js
+++
@@ -1,1 +0,0 @@
-import{q as r,a as e}from"./app-be3d077c.js";import{B as s}from"./Breadcrumbs-b2a4e449.js";const i=()=>{const a=r().props;return e("div",{className:"catalogue-categories",children:e("div",{className:"container-outer",children:e("div",{className:"catalogue-categories__outer",children:e("div",{className:"catalogue-categories__inner",children:e(s,{...a})})})})})};export{i as T};
